c048435b62e1f31d2bf0d1c3db630f7e0b9232fe
SRIKRISHNA S KASHYAP <skashyapsri04@gmail.com>
2020-11-30 09:51:47 +0530

    Update config.js

--- Frontend/admin-frontend/src/config/config.js
+++ Frontend/admin-frontend/src/config/config.js
@@ -1,16 +1,8 @@
 export default {
-	apiGateway: {
-		REGION: 'ap-south-1',
-		URL: 'https://api.weddingvia.com'
-	},
 	cognito: {
 		REGION: 'ap-south-1',
 		USER_POOL_ID: 'ap-south-1_6WdK18rGs',
 		APP_CLIENT_ID: '3spi7v93b04tbp0ngqnl87b2mb',
 		IDENTITY_POOL_ID: 'ap-south-1:a0469837-c6d8-46cc-bce6-dddd1aee8515'
-	},
-	storage:	{
-		REGION: 'ap-south-1',
-		BUCKET_NAME: 'weddingvia-user-images'
 	}
 };
